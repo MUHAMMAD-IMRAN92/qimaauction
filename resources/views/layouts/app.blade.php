a72e79b827ca8d992191468f5d04924dbf581e60
imranskylinx <imran.skylinxtech@gmail.com>
2022-04-18 15:39:26 +0500

    jury id cahnges in migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Qima Auction') }}</title>
+    <title>Qima Auction</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
