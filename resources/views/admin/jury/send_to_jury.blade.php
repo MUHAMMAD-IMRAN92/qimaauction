42d5b3ea5df0e38bfce14d24e63a6903a516cdc0
Faizan Chughtai <chughtaifaizan@gmail.com>
2022-06-07 02:54:41 +0500

    product issues

--- resources/views/admin/jury/send_to_jury.blade.php
+++ resources/views/admin/jury/send_to_jury.blade.php
@@ -184,55 +184,7 @@
 
                                                     @endforeach
 
-                                                    <div class="row">
-                                                        <div class="col-md-4">
-                                                                @foreach ($products as $key => $product)
-                                                                <input type="hidden" name="products[]" value="{{ $product->id }}">
-                                                                    <input type="text" class="form-control"  id="products" value="{{ $product->product_title }}">
-                                                                @endforeach
-                                                        </div>
                                                     
-                                                        <div class="col-md-2">
-                                                            @foreach ($products as $key => $product)
-                                                            <input type="text" class="form-control"  name="samples[]" id="samples" value="{{$product->sample}}" placeholder="Enter Sample Id">
-                                                        @endforeach
-                                                           
-                                                        </div>
-                                                        <div class="col-md-2">
-                                                            @foreach ($products as $key => $product)
-                                                            <input type="number" class="form-control" name="postion[]" id="postion" placeholder="Postion" value="{{$product->postion}}" oninput="if (this.value > 9) this.value = 0;">
-                                                            @endforeach
-                                                        </div>
-                                                        <div class="col-md-4">
-                                                            @foreach ($products as $key => $product)
-                                                               <div class="row checkbox-pad">
-                                                                <div class="chk">
-                                                                    <label class="check-label" for="male"> <b>T1</b> </label>
-                                                                    <input type="radio" name="{{$key}}" value="1" class="chk1 pt-5" {{ $product->table == 1 ? 'checked' : '' }} >
-                                                                </div>
-                                                                <div class="chk">
-                                                                    <label class="check-label" for="male"> <b>T2</b> </label>
-                                                                    <input type="radio" name="{{$key}}" value="2" class="chk2 pt-5" {{ $product->table == 2 ? 'checked' : '' }}>
-                                                                </div>
-                                                                 <div class="chk">
-                                                                    <label class="check-label" for="male"> <b>T3</b> </label>
-                                                                    <input type="radio" name="{{$key}}" value="3" class="chk3 pt-5" {{ $product->table == 3 ? 'checked' : '' }}>
-                                                                 </div>
-                                                                 <div class="chk">
-                                                                    <label class="check-label" for="male"> <b>T4</b> </label>
-                                                                    <input type="radio"  name="{{$key}}" value="4" class="chk4 pt-5" {{ $product->table == 4 ? 'checked' : '' }}>
-                                                                 </div>
-                                                                 <div class="chk">
-                                                                    <label class="check-label" for="male"> <b>T5</b> </label>
-                                                                    <input type="radio"  name="{{$key}}" value="5" class="chk4 pt-5" {{ $product->table == 5 ? 'checked' : '' }}>
-                                                                 </div>
-                                                               </div>
-                                                                 @endforeach
-                                                         </div>
-                                                        {{-- <div class="pt-2">
-                                                            <button type="button" class="btn btn-primary btn-sm ml-1" id="save">Save</button>
-                                                        </div> --}}
-                                                    </div>
                                                     @error('products')
                                                     <div class="alert alert-danger">{{ $message }}</div>
                                                     @enderror
