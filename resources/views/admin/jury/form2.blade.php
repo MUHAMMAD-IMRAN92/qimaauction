787037b3ac45505ada45e84e0845ff4ae6406368
Faizan Chughtai <chughtaifaizan@gmail.com>
2022-06-08 18:25:49 +0500

    form changes

--- resources/views/admin/jury/form2.blade.php
+++ resources/views/admin/jury/form2.blade.php
@@ -483,6 +483,9 @@ data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
                                                 <ul class="breadcrumb">
                                                     <li><a href="#"><p class="" style="font-family: 'Montserrat';font-size:25px; padding-top:0.5rem; color: #A4A3A3;">TABLE-{{$productdata->table}}</p></a></li>
                                                     <li><a href="#"><p class="" style="font-family: 'Montserrat';font-size:25px;color: #A4A3A3;">POSITION-{{$productdata->postion}}</p></a></li>
+                                                    <input type="hidden" name="current_position" value="{{$productdata->postion}}">
+                                                    <input type="hidden" name="next_position" value="{{$productdata->postion++}}">
+                                                    <input type="hidden" name="previous_position" value="{{$productdata->postion--}}">
                                                 </ul>
                                             </div>
                                             <!--Breadcrumb Section-->
