33e6e9e4d56750b20870920bd58cbbcbb91e823a
Faizan Chughtai <chughtaifaizan@gmail.com>
2022-06-08 19:07:03 +0500

    navigation

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -193,7 +193,7 @@ class ReviewController extends Controller
             $sample2Sent = SentToJury::
                         where('sample_sent_to_jury.jury_id', $request->jury_id)
                         ->where('sample_sent_to_jury.tables', $request->table_value)
-                        ->where('postion',$request->current_position-1);
+                        ->where('postion',$request->current_position-1)
                          ->first();
                         dd($sample2Sent);
                         if($sample2Sent){
