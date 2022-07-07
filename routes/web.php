1488c390bf09c281c85cee04aca79799800e9f86
Faizan Chughtai <chughtaifaizan@gmail.com>
2022-07-07 15:24:47 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // Route::view('/', 'customer.dashboard.index');
-Route::post('signuo',[App\Http\Controllers\AuctionController::class, 'newslettersignup']);
+Route::post('signup',[App\Http\Controllers\AuctionController::class, 'newslettersignup']);
 Route::get('/', [App\Http\Controllers\AuctionController::class, 'winningCoffee']);
 // Route::view('/index-new', 'customer.dashboard.index-new');
 Route::get('/index-new', [App\Http\Controllers\AuctionController::class, 'winningCoffee']);
