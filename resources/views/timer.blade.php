0b7a8c64c86373f203642168d692096d206de01e
Faizan Chughtai <chughtaifaizan@gmail.com>
2022-08-11 22:31:17 -0400

    sidebar issue

--- resources/views/timer.blade.php
+++ resources/views/timer.blade.php
@@ -331,7 +331,7 @@
             <a href="https://www.facebook.com/qimacoffee/"><i class="fa fa-facebook"></i></a>
             <a href="https://www.linkedin.com/company/qima-coffee/mycompany/"><i class="fa fa-linkedin" aria-hidden="true"></i> </a>
 
-            <a href="https://www.youtube.com/channel/UCcgmMB11TkfAsGO1uiHuKnQ"><i class="fa fa-youtube" aria-hidden="true"></i> </a>
+            <a href="https://www.youtube.com/channel/UCcgmMB11TkfAsGO1uiHuKnQ"><i class="fa fa-youtube-play" aria-hidden="true"></i> </a>
         </div>
     </section>
     <section>
