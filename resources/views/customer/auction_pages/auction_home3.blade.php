034a0c509383f906b8383eb42d055b1b6f0392c5
Faizan Chughtai <chughtaifaizan@gmail.com>
2022-08-12 09:38:04 -0400

    agreement

--- resources/views/customer/auction_pages/auction_home3.blade.php
+++ resources/views/customer/auction_pages/auction_home3.blade.php
@@ -1129,6 +1129,7 @@
         </div>
     </section> --}}
     <div class="container box text-center section-4-text-1 auction_pending" style="display: none;width:auto;">
+        <h2 style="text-align: center">This is a Practice Auction.</h2>
         <div class="row boxrow">
             {{ $auction->startDate }}
             {{ date('Y-m-d H:i:s') }}
