49d701143e1f5f7420806a83af2d6be843d2d205
unknown <ijzahmed8@gmail.com>
2022-08-18 17:35:16 +0500

    Working on changes

--- resources/views/customer/auction_pages/auction_winners.blade.php
+++ resources/views/customer/auction_pages/auction_winners.blade.php
@@ -1124,7 +1124,7 @@
     </div> --}}
     <section>
         <h2 style="text-align: center;font-family:'Montserrat';">Welcome to the best of Yemen.</h2>
-
+        <h3 style="text-align: center;font-family:'Montserrat';">Auction starting soon.</h3>
         <div class="table-container">
             <div class="tab-content" id="nav-tabContent">
                 <nav class="tablenav">
