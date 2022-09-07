24d0d34e7dc4294e09a4139421e9dd9e91866cab
Talha hussain <talha.skylinxtech@gmail.com>
2022-09-07 11:13:03 +0500

    added model and migrations for group bidding

---
+++ database/migrations/2022_09_07_060433_create_offers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('auction_id');
+            $table->integer('auction_product_id');
+            $table->decimal('amount');
+            $table->decimal('weight');
+            $table->integer('is_active')->default(1);
+            $table->integer('paddle_number')->nullable();
+            $table->string('expired_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+}
---
+++ database/migrations/2022_09_07_061107_create_user_offers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_offers', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('offer_id');
+            $table->string('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_offers');
+    }
+}
