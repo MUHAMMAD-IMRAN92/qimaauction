1003d5b62640fb00115b2585f67e5f89faa2f0c4
Noman Khalid <nomankhalid.skylinxtech@gmail.com>
2022-09-20 07:19:39 +0000

    Socketforgroupbidding

--- resources/views/customer/auction_pages/homejs.blade.php
+++ resources/views/customer/auction_pages/homejs.blade.php
@@ -6,7 +6,7 @@
             clearTimeout(timerId);
         }
         var curnet_time=new Date();
-        var curnet_time = AddMinutesToDate(curnet_time,300);
+        var curnet_time = AddMinutesToDate(curnet_time,3000);
         var end_time=new Date(endtime);
         var seconds = end_time - curnet_time;
         var dif = (seconds/1000)/60;
@@ -31,6 +31,7 @@ function countdown() {
 }
 
 function doSomething(offer_id) {
+
             $.ajax({
                 url: "{{ route('groupbidupdateStatus') }}",
                 method: 'POST',
@@ -40,22 +41,13 @@ function doSomething(offer_id) {
                 },
                 success: function(response) {
                             var my=response;
-                            if(my.length == undefined)
-                            {
-                                $('#offers').empty();
-                                $('#other-offers').empty();
-                                $('#bag_quantity').val('');
-                                $('#bid_amount').val('');
-                                $('.show-bid-confirm').show();
-                                $('.liabiltysec').hide();
-                            }
                             var offersdata=response.offersdata
                             socket.emit('add_groupbid_updates', {
                              "offersdata": offersdata,
                             //  "adminofferData":adminofferData,
 
                         });
-                            // console.log(offersdata);
+                            console.log(offersdata);
                     },
                     error: function(error) {
                         console.log(error)
