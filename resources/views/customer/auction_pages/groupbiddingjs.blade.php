4f45820df5a2214d2b0ef5a3273741525153bf36
Noman Khalid <nomankhalid.skylinxtech@gmail.com>
2022-09-27 07:46:05 +0000

    Latestbugfixesingroupbidding

--- resources/views/customer/auction_pages/groupbiddingjs.blade.php
+++ resources/views/customer/auction_pages/groupbiddingjs.blade.php
@@ -24,6 +24,12 @@
             },
             success: function(response) {
                 var my = response.groupbid;
+                const interval_id = window.setInterval(function(){}, Number.MAX_SAFE_INTEGER);
+
+// Clear any timeout/interval up to that id
+        for (let i = 1; i < interval_id; i++) {
+             window.clearInterval(i);
+            }
                 if (my.length != 0) {
                     $('#offers').empty();
                     $('#other-offers').empty();
