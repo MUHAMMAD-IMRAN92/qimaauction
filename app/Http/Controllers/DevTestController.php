aba6cc586b8c13886878d901e2bb18790ae46efd
imranskylinx <imran.skylinxtech@gmail.com>
2023-06-16 00:04:38 +0500

    dev test

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DevTestController extends Controller
 {
     public function index()
     {
+
+  return      Product::where('product_title' , 'Hifthallah Alhaymi')->where('sample_id' , 1206)->first();
+
         return phpinfo();
         // return view('admin.dashboard.index');
         $en = encrypt(1);
