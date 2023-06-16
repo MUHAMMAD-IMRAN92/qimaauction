7c7e884686a3948950d73e270c582108d8e9c63f
imranskylinx <imran.skylinxtech@gmail.com>
2023-06-16 18:46:36 +0500

    form2

--- resources/views/admin/jury/form2.blade.php
+++ resources/views/admin/jury/form2.blade.php
@@ -2634,7 +2634,7 @@
                 calcTotal();
                 if (chkmanual) {
                     toggleDivs();
-                    if ("$reviewdata")
+                    if ($reviewdata)
                         $('input[name=total_score]').val("{{ $reviewdata->total_score }}");
 
                 }
