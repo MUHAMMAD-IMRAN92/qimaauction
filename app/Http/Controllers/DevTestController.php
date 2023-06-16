07dae3089d8b4967941e375e171564181b915b17
imranskylinx <imran.skylinxtech@gmail.com>
2023-06-16 19:46:21 +0500

    position asc and jury form fixes

--- app/Http/Controllers/DevTestController.php
+++ app/Http/Controllers/DevTestController.php
@@ -10,7 +10,7 @@ class DevTestController extends Controller
 {
     public function index()
     {
-        return Product::where('sample', 2610)->first();
+        return Product::where('sample', 2610)->delete();
         return phpinfo();
 
         $en = encrypt(1);
