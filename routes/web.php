c801690e25b6ba58d0cdcf2566ef0633e68990b5
imranskylinx <imran.skylinxtech@gmail.com>
2023-07-07 16:32:19 +0500

    product detail  image

--- routes/web.php
+++ routes/web.php
@@ -310,8 +310,3 @@ Route::get('/bid_agreement', function () {
     return redirect('public/bidding_agreement.pdf');
 }); //'customer/pages/bid_agreement');
 Route::post('/accept-agrements', [App\Http\Controllers\AgreementController::class, 'acceptAgreement']);
-Route::get('/del', function () {
-    AuctionProductImages::truncate();
-
-    return 'done';
-});
