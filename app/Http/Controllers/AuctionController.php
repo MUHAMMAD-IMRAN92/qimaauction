793295cc3603dca82af2903660370d224205b4c7
imranskylinx <imran.skylinxtech@gmail.com>
2023-07-14 11:17:39 +0500

    testing

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -41,9 +41,8 @@ class AuctionController extends Controller
         $auctionAlchemyWinning = collect();
         $auction = Auction::where('is_active', '1')->first();
         if ($auction) {
-
             $auctionNaturalWinning = AuctionProduct::where('auction_id', $auction->id)->whereIn('process', ['Natural', 'DEEP FERMENTATION', 'Slow Dried'])->where('home_page', 1)->orderBy('rank', 'asc')->get();
-            $auctionAlchemyWinning = AuctionProduct::where('auction_id', $auction->id)->whereIn('process', ['Alchemy'])->orderBy('rank', 'asc')->where('home_page', 1)->get();
+          return  $auctionAlchemyWinning = AuctionProduct::where('auction_id', $auction->id)->whereIn('process', ['Alchemy'])->orderBy('rank', 'asc')->where('home_page', 1)->get();
         }
         return view('admin.dashboard', [
             'natural' => $auctionNaturalWinning,
